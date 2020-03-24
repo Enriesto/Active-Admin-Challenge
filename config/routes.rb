957751834fb65721ca8b308129de32f6d25b0fb9
Ernest Janzen <ejjthehun@hotmail.com>
2020-03-24 16:07:26 -0500

    Successfully appended images to each customer.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  # devise_for :admin_users, ActiveAdmin::Devise.config
+  # ActiveAdmin.routes(self)
+  root to: 'customers#index'
+  get 'customers/alphabetized'
+  get 'customers/missing_email'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
